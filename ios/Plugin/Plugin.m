40e2814dafea5edf158238ea79653fcbb1dfbc7a
Derrick Gall <derrick.gall@zubie.com>
2021-10-29 20:18:24 -0500

    Implemented unzip API for iOS

--- ios/Plugin/Plugin.m
+++ ios/Plugin/Plugin.m
@@ -1,8 +1,9 @@
 #import <Foundation/Foundation.h>
 #import <Capacitor/Capacitor.h>
+#import <ZIPFoundation/ZIPFoundation-umbrella.h>
 
 // Define the plugin using the CAP_PLUGIN Macro, and
 // each method the plugin supports using the CAP_PLUGIN_METHOD macro.
 CAP_PLUGIN(CapacitorZip, "CapacitorZip",
-           CAP_PLUGIN_METHOD(echo, CAPPluginReturnPromise);
+    CAP_PLUGIN_METHOD(unZip, CAPPluginReturnPromise);
 )
--- ios/Plugin/Plugin.swift
+++ ios/Plugin/Plugin.swift
@@ -1,5 +1,6 @@
 import Foundation
 import Capacitor
+import ZIPFoundation
 
 /**
  * Please read the Capacitor iOS Plugin Development Guide
@@ -13,5 +14,38 @@ public class CapacitorZip: CAPPlugin {
         call.success([
             "value": value
         ])
+    }
+
+    @objc func unZip(_ call: CAPPluginCall) {
+        let sourcePath = call.getString("sourcePath") ?? ""
+        let sourceDirectory = call.getString("sourceDirectory") ?? ""
+        let destinationPath = call.getString("destinationPath") ?? ""
+        let destinationDirectory = call.getString("destinationDirectory") ?? ""
+        
+        let sourceURL = FilesystemUtils.getFileUrl(sourcePath, sourceDirectory)
+        let destinationURL = FilesystemUtils.getFileUrl(destinationPath, destinationDirectory)
+        
+        if (sourceURL == nil) {
+            call.reject("Zip source file not found")
+            return;
+        }
+        
+        if (destinationURL == nil) {
+            call.reject("Cannot unzip file at given destination")
+            return;
+        }
+        
+        let fileManager = FileManager()
+        do {
+            try fileManager.createDirectory(at: destinationURL!, withIntermediateDirectories: true, attributes: nil)
+            try fileManager.unzipItem(at: sourceURL!, to: destinationURL!)
+            
+            call.resolve([
+                "path": destinationPath,
+                "directory": destinationDirectory
+            ])
+        } catch {
+            call.reject(error.localizedDescription);
+        }
     }
 }
